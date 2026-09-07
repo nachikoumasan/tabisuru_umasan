35d1f7205dfbdd76039539f8f4140eea7758023e
nachikoumasan <nachiko.umasan0215@gmail.com>
2026-09-07 22:29:05 +0900

    Delete echo_again_test_v1 directory

--- echo_again_test_v1/assets/js/core/ui/pageListInit.js
+++
@@ -1,8 +0,0 @@
-import { initPageListModal } from './pageListModal.js';
-
-window.addEventListener('DOMContentLoaded', () => {
-  const counter = document.getElementById('page-counter');
-  if (counter) {
-    initPageListModal({ mode: 'modal', showUnvisitedTitle: false, pageHref: '' });
-  }
-});
